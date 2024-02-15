6c03b12b4e8ee143da28be0fb40979f8e23cea3c
Jason Kai <jasonkai7@gmail.com>
2024-02-15 15:09:09 -0500

    Remove extra import

--- afids_cnn/train.py
+++ afids_cnn/train.py
@@ -12,8 +12,6 @@ from keras.layers import BatchNormalization, LeakyReLU
 
 from afids_cnn.generator import customImageDataGenerator
 
-from afids_cnn.generator import customImageDataGenerator
-
 def gen_training_array(
     num_channels: int,
     dims: NDArray,
